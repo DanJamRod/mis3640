c2ecc407f2492661c77bbd121e31d0611ca546ad
DanJamRod <rodgersd09@gmail.com>
2020-01-23 15:42:16 -0500

    Update hello.py

--- session01_02/hello.py
+++ session01_02/hello.py
@@ -2,6 +2,10 @@
 # name = input(f"Please enter your name >>")
 # print(f"Hello, {name}")
 
-# Print message
-message = f"I did something cool today!"
-print(f"{message}")
+# # Print message
+# message = f"I did something cool today!"
+# print(f"{message}")
+
+name = f"Daniel"
+name = name + f" James Rodgers"
+print(name)
