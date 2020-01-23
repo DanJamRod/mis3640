ac82021487d4ab5b37aec9ff8fe5b5b65518673a
DanJamRod <rodgersd09@gmail.com>
2020-01-23 15:37:32 -0500

    Update session01_02

---
+++ session01_02/demo_02.py
@@ -0,0 +1,7 @@
+print(f"Hello, world!")
+
+print(f"Hey Jude, don't make it bad")
+print(f'Hey Jude, don\'t make it bad')
+
+name = input()
+print(f"{name}")
---
+++ session01_02/hello.py
@@ -0,0 +1,7 @@
+# # Input and print name
+# name = input(f"Please enter your name >>")
+# print(f"Hello, {name}")
+
+# Print message
+message = f"I did something cool today!"
+print(f"{message}")
