d5ad2849d53e05b714b527a5a870d83bf5b0b487
DanJamRod <rodgersd09@gmail.com>
2020-01-23 15:16:56 -0500

    Update hello.py

--- session01_02/hello.py
+++ session01_02/hello.py
@@ -1,1 +1,7 @@
 print(f"Hello, world")
+
+# print(f"Hello, world"
+# # If no 2nd parentheses, "SyntaxError: unexpected EOF while parsing"
+
+# print(f"Hello, world)
+# # If no 2nd quotation, "SyntaxError: EOL while scanning string literal"
