4ec90dfddeeca488faeb8f651a63c0a3a5bc3002
DanJamRod <rodgersd09@gmail.com>
2020-02-04 17:56:06 -0500

    Create my_polyline_test.py

---
+++ session05/my_polyline_test.py
@@ -0,0 +1,13 @@
+import turtle
+import math
+import my_polyline
+
+t = turtle.Turtle()
+
+# my_polyline.polyline(t, n=3, length =100, angle=50)
+# my_polyline.polygon(t, n=5, length=100)
+# my_polyline.square(t, length=100)
+# my_polyline.arc(t, r=100, angle=270)
+# my_polyline.circle(t, r=100)
+
+turtle.mainloop()
