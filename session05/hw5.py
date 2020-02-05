4cc7fae60520844a5c2f1523406781c45a986024
DanJamRod <rodgersd09@gmail.com>
2020-02-04 19:44:44 -0500

    Update hw5.py

--- session05/hw5.py
+++ session05/hw5.py
@@ -5,11 +5,49 @@ import math
 import my_polyline
 
 t = turtle.Turtle()
-for i in range (1):
-    my_polyline.arc(t, 300, 40)
-    my_polyline.arc(t, 0, 140)
-    my_polyline.arc(t, 300, 40)
-    my_polyline.arc(t, 0, 80)
 
+def petals():
+    for i in range (3):
+        my_polyline.arc(t, r=200, angle=60)
+        my_polyline.arc(t, r=0, angle=120)
+        my_polyline.arc(t, r=200, angle=60)
+
+def flower():
+    petals()
+    my_polyline.arc(t, r=0, angle=60)
+    petals()
+    my_polyline.move(t, 0, -200)
+    my_polyline.arc(t, r=0, angle=300)
+    my_polyline.circle(t, r=200)
+
+def yin_yang():
+    my_polyline.arc(t, r=60, angle=180)
+    my_polyline.circle(t, 120)
+    my_polyline.move(t, 0, 0)
+    my_polyline.arc(t, r=60, angle=180)
+    my_polyline.move(t, 0, 50)
+    my_polyline.circle(t, 10)
+    my_polyline.move(t, 0, -70)
+    my_polyline.circle(t, 10)
+
+def tri_circ():
+    my_polyline.polyline(t,n=1,length=0, angle=-30)
+    my_polyline.polyline(t,n=1,length=200, angle=120)
+    my_polyline.polyline(t,n=1,length=100, angle=0)
+    my_polyline.circle(t, 200*3**0.5/6)
+    my_polyline.polyline(t,n=1,length=100, angle=120)
+    my_polyline.polyline(t,n=1,length=200, angle=60)
+
+def illuminati():
+    for i in range (4):
+        tri_circ()
+    my_polyline.move(t, 0, -200)
+    my_polyline.circle(t,200)
+
+
+
+# flower()
+# yin_yang()
+illuminati()
 
 turtle.mainloop()
