c65621e9ab6452c9fcf0c2bafc1943e7170ea2f8
DanJamRod <rodgersd09@gmail.com>
2020-02-04 22:21:12 -0500

    Update my_polyline_test.py

--- session05/my_polyline_test.py
+++ session05/my_polyline_test.py
@@ -4,6 +4,7 @@ import my_polyline
 
 t = turtle.Turtle()
 
+# my_polyline.turn(t, angle=180)
 # my_polyline.polyline(t, n=3, length =100, angle=50)
 # my_polyline.polygon(t, n=5, length=100)
 # my_polyline.square(t, length=100)
