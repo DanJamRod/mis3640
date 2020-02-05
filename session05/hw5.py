472fd174edb11dce4914e2d8513848e892d9e27c
DanJamRod <rodgersd09@gmail.com>
2020-02-04 22:21:15 -0500

    Update hw5.py

--- session05/hw5.py
+++ session05/hw5.py
@@ -1,4 +1,5 @@
 print(f"Daniel James Rodgers HW5\n")
+# Lines 50 - 52 have the functions 
 
 import turtle
 import math
@@ -9,15 +10,15 @@ t = turtle.Turtle()
 def petals():
     for i in range (3):
         my_polyline.arc(t, r=200, angle=60)
-        my_polyline.arc(t, r=0, angle=120)
+        my_polyline.turn(t, angle=120)
         my_polyline.arc(t, r=200, angle=60)
 
 def flower():
     petals()
-    my_polyline.arc(t, r=0, angle=60)
+    my_polyline.turn(t, angle=60)
     petals()
     my_polyline.move(t, 0, -200)
-    my_polyline.arc(t, r=0, angle=300)
+    my_polyline.turn(t, angle=300)
     my_polyline.circle(t, r=200)
 
 def yin_yang():
@@ -31,7 +32,7 @@ def yin_yang():
     my_polyline.circle(t, 10)
 
 def tri_circ():
-    my_polyline.polyline(t,n=1,length=0, angle=-30)
+    my_polyline.turn(t, angle=-30)
     my_polyline.polyline(t,n=1,length=200, angle=120)
     my_polyline.polyline(t,n=1,length=100, angle=0)
     my_polyline.circle(t, 200*3**0.5/6)
@@ -45,9 +46,9 @@ def illuminati():
     my_polyline.circle(t,200)
 
 
-
+# # Uncomment to run a program
 # flower()
 # yin_yang()
-illuminati()
+# illuminati()
 
 turtle.mainloop()
