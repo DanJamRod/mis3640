6f46f440196dce74ae1993640bbc9e6a21dce843
DanJamRod <rodgersd09@gmail.com>
2020-02-04 22:20:57 -0500

    Update hello.py

--- session01/hello.py
+++ session01/hello.py
@@ -1,16 +1,15 @@
-# # Input and print name
-# name = input(f"Please enter your name >>")
-# print(f"Hello, {name}")
+# Input and print name
+name = input(f"Please enter your name >>")
+print(f"Hello, {name}")
 
-# # Print message
-# message = f"I did something cool today!"
-# print(f"{message}")
+# Print message
+message = f"I did something cool today!"
+print(f"{message}")
 
-# # Edit variable and print type
-# name = f"Daniel"
-# name = name + f" James Rodgers"
-# print(name)
-# print(type(name))
-# name = 123
-# print(type(name))
-
+# Edit variable and print type
+name = f"Daniel"
+name = name + f" James Rodgers"
+print(name)
+print(type(name))
+name = 123
+print(type(name))
