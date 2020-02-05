5e99a237cd947d48e14caf4296582ce8e457bb86
DanJamRod <rodgersd09@gmail.com>
2020-02-04 22:21:02 -0500

    Update my_abs.py

--- session04/my_abs.py
+++ session04/my_abs.py
@@ -2,14 +2,13 @@ print(f"Daniel James Rodgers HW4a\n")
 
 print(f"my_abs")
 """
-number:an integer or a floating number
-return the absolute value of a number
+returns the absolute value of an int, float, or complex number
 """
 
 def my_abs(x):
     if isinstance(x,(bool)): # Special handling as bool is a sub class of int. 
         return(f"It is not possible to find the absolute value of '{x}' as it is a boolean and not an integer or a float")
-    elif isinstance(x,(complex)): # Special handling as complex is a sub class of float. 
+    elif isinstance(x,(complex)): # Special handling of complex
         return(f"The absolute value of {x} is {((x.real)**2 + ((x.imag)**2))**0.5}")
     elif not isinstance(x,(int,float)):
         return(f"It is not possible to find the absolute value of '{x}' as it is a {type(x)} and not an integer or a float")    
@@ -21,6 +20,7 @@ def my_abs(x):
 # # Test
 # print(my_abs(5))
 # print(my_abs(-2.5))
+# print (my_abs([1, 5, 9]))
 # print(my_abs(f"Hello, world!"))
-# print(my_abs(5 == 5)) # Shows the importance of testing, would never have known bool or complex are a sub class of int and float respectively if hadn't have tested this and gotten weird results
+# print(my_abs(5 == 5))
 # print(my_abs(complex(-3,4)))
