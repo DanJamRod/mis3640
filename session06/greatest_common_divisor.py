a83fa07376a8d802e37fcf3d7d3328af52587822
DanJamRod <rodgersd09@gmail.com>
2020-02-10 02:44:27 -0500

    Create greatest_common_divisor.py

---
+++ session06/greatest_common_divisor.py
@@ -0,0 +1,15 @@
+def gcd(a,b):
+    """
+    Return the gcd of a and b
+    """
+    if not (isinstance(a, int) and isinstance(b, int)):
+        return "Incorrect input"
+    elif b == 0:
+        return a      
+    else:
+        return gcd(b,a%b)
+
+print(gcd(2, 12))
+print(gcd(6, 12))
+print(gcd(9, 12))
+print(gcd(17, 12))
