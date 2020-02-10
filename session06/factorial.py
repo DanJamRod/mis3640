514dbef3c6ff65c5eb2cbab19fd6ea46f96cea64
DanJamRod <rodgersd09@gmail.com>
2020-02-10 02:44:20 -0500

    Create factorial.py

---
+++ session06/factorial.py
@@ -0,0 +1,17 @@
+def factorial(n):
+    if not isinstance(n, int):
+        return "Incorrect input"
+    elif n < 0:
+        return "Incorrect input"
+    else:
+        fact = 1
+        for i in range(1,n+1): 
+            fact = fact * i
+        return fact
+
+print(factorial(1))
+print(factorial(2))
+print(factorial(3))
+print(factorial(19))
+print(factorial(-1))
+print(factorial("hello"))
