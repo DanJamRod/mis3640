16b53647372be4f98b050799adf0a7146a545c1c
DanJamRod <rodgersd09@gmail.com>
2020-02-11 14:27:58 -0500

    Update greatest_common_divisor.py

--- session06/greatest_common_divisor.py
+++ session06/greatest_common_divisor.py
@@ -9,7 +9,7 @@ def gcd(a,b):
     else:
         return gcd(b,a%b)
 
-print(gcd(2, 12))
-print(gcd(6, 12))
-print(gcd(9, 12))
-print(gcd(17, 12))
+# print(gcd(2, 12))
+# print(gcd(6, 12))
+# print(gcd(9, 12))
+# print(gcd(17, 12))
