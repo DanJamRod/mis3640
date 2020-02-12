ab4e5da40d4b09d69f30533e85416a0a65ca4b9e
DanJamRod <rodgersd09@gmail.com>
2020-02-11 23:31:13 -0500

    Create while_07.py

---
+++ session07/while_07.py
@@ -0,0 +1,9 @@
+i = 1
+current_sum = 0
+
+while i <= 10:
+    print(f"current value of i is {i}")
+    current_sum += i
+    i += 1
+    print (f"after adding i, the current sum is {current_sum}")
+    print()
