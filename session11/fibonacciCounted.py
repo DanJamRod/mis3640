fcf43bcff265086569b44878586a823b26a9bdd8
DanJamRod <rodgersd09@gmail.com>
2020-02-27 22:12:44 -0500

    Create fibonacciCounted.py

---
+++ session11/fibonacciCounted.py
@@ -0,0 +1,36 @@
+def fib(n):
+    global number_fib_calls
+    number_fib_calls += 1
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+known = {1: 1, 2: 2}
+
+
+def fib_efficient(n):
+    global number_fib_calls
+    number_fib_calls += 1
+    if n in known:
+        return known[n]
+    else:
+        ans = fib_efficient(n - 1) + fib_efficient(n - 2)
+        known[n] = ans
+        return ans
+
+
+number_fib_calls = 0
+fib_args = 10
+
+print(fib(fib_args))
+print('function calls', number_fib_calls)
+
+number_fib_calls = 0
+
+
+print(fib_efficient(fib_args))
+print('function calls', number_fib_calls)
